9c475577693208e815c825416c3a0cc0c456ab2d
Nail Ismagilov <nailismagilov555@gmail.com>
2024-04-16 20:31:17 +0200

    refactor: global refactor of the main function.

--- main.py
+++ main.py
@@ -1,81 +1,38 @@
 #!/usr/bin/python
 from dogs import *
-from doglist import dogList
+from doglist import *
 from cli import *
+from manage_folder import *
+from report import *
 
-REPORT_PATH = f"{pathlib.Path(__file__).parent.resolve()}/report"
-DOGS_PATH = f"{pathlib.Path(__file__).parent.resolve()}/hunde"
-
-
+dogsGender = ["Hündinen", "Rüden", "Welpen_Madchen", "Welpen_und_Junghunde"]
 
 # main loop
-
-
-
-def create_report(gender, all=False):
-    """Creates a report and saves it to a file.
-
-    Args:
-        path (str): The path to the directory where the report files are stored.
-        gender (str): The gender of the dogs to include in the report.
-        all (bool): Whether to print the output to the console or not.
-
-    Returns:
-        None
-    """
-    report = f"{REPORT_PATH}/{gender}_report.txt"
-    pflegestelle = f"{REPORT_PATH}/pflegestelle_report.txt"
-
-    with open(report, "w", encoding="utf-8") as file:
-        file.write("GEBLIEBEN:\n")
-        for gender in vermittlung.keys():
-            # file.write(f"  {gender}\n")
-            dogs = [dog_name for dog_name, dog_gender in OLD_DOG.items() if dog_gender == gender]
-            for dog in dogs:
-                file.write("    * " + dog + "\n")
-
-        file.write("\nABGEHOLT:\n")
-        for gender in vermittlung.keys():
-            # file.write(f"  {gender}\n")
-            dogs = [dog_name for dog_name, dog_gender in HAPPY_DOG.items() if dog_gender == gender]
-            for dog in dogs:
-                file.write("    * " + dog + "\n")
-
-        file.write("\nNEUE:\n")
-        for gender in vermittlung.keys():
-            # file.write(f"  {gender}\n")
-            dogs = [dog_name for dog_name, dog_gender in NEW_DOG.items() if dog_gender == gender]
-            for dog in dogs:
-                file.write("    * " + dog + "\n")
-        
-    if not all:
-        print_output(report)
-
-def clean_report():
-    """ 
-    """
-    reports = [f for f in os.listdir(REPORT_PATH) if os.path.isfile(os.path.join(REPORT_PATH, f))]
-    for report in reports:
-        with open(report, "r+") as file:
-            file.truncate(0)
-
-def delete_happy_dogs()
 def start():
     """Main method calling other methods"""
     user_choice = "0"
-    # Dictionary to store the mapping between the user input and the dog gender
-    
+
     # create dogs list
+    dogList = Doglist()
+
     # create folders
+    create_new_dogs()
+
     # deflete folders
-    # create reports
-   
-    dogList.happy_dogs()
+    for gender in dogList.happy_dogs().keys():
+        for dog in dogList.happy_dogs()[gender]:
+            delete_dog(gender, dog)
+
+    # create reports   
+    create_report(dogList)
+    
+    # create CLI object
     cli = CLI()
+
     while (user_choice.upper() != EXIT):
-        cli.create_dog()
         cli.create_menu()
         user_choice = input("Select Dog: ")
+        if user_choice.upper() != EXIT and int(user_choice) <= len(dogsGender):
+            show_report(dogsGender[int(user_choice)-1])
 
-       
 start()
